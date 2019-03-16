ab918f91d1d83056adba796d71fc0778db0d4de0
Daniel Lee <danman1188@gmail.com>
2019-03-15 21:31:24 -0600

    feat(captain-america): add docker support
    Setup and build the server docker image.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,30 @@
 #! /usr/bin/env bash
 
-# TODO: make this a function
+set -u -e -o pipefail
 
+readonly work_dir=$(pwd)
+
+echo "######################"
+echo "Building all packages"
+echo "######################"
+
+# build out all the packages
 npx lerna run build
+
+echo ""
+echo "######################"
+echo "Building docker images"
+echo "######################"
+
+echo ""
+echo "building captain-america"
+docker build \
+  -f ${work_dir}/packages/captain-america/Dockerfile \
+  -t captain-america:latest \
+  ${work_dir}/packages/captain-america
+
+echo ""
+echo "######################"
+echo "COMPLETE!"
+echo "######################"
+exit 0
